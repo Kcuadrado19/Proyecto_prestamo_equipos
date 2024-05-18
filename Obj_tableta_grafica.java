ff4d854f0052877232933ccaf865cf914e162cd2
Kellys Cuadrado Barboza <cuadradokellys19@gmail.com>
2024-05-18 15:18:18 -0500

    See creo el objeto estufiante diseño y tableta gráfica

---
+++ Obj_tableta_grafica.java
@@ -0,0 +1,78 @@
+public class Obj_tableta_grafica {
+
+        private String serial;
+        private String marca;
+        private float tamaño;
+        private float precio;
+        private int almacenamiento;
+        private float peso;
+
+//Constructor
+    
+        public Obj_tableta_grafica(String serial, String marca, float tamaño, float precio, int almacenamiento, float peso) {
+            this.serial = serial;
+            this.marca = marca;
+            this.tamaño = tamaño;
+            this.precio = precio;
+            this.almacenamiento = almacenamiento;
+            this.peso = peso;
+        }
+
+//Getter y setter
+
+        public String getSerial() {
+            return serial;
+        }
+
+        public void setSerial(String serial) {
+            this.serial = serial;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public float getTamaño() {
+            return tamaño;
+        }
+
+        public void setTamaño(float tamaño) {
+            this.tamaño = tamaño;
+        }
+
+        public float getPrecio() {
+            return precio;
+        }
+
+        public void setPrecio(float precio) {
+            this.precio = precio;
+        }
+
+        public int getAlmacenamiento() {
+            return almacenamiento;
+        }
+
+        public void setAlmacenamiento(int almacenamiento) {
+            this.almacenamiento = almacenamiento;
+        }
+
+        public float getPeso() {
+            return peso;
+        }
+
+        public void setPeso(float peso) {
+            this.peso = peso;
+        }
+
+
+
+
+
+        
+
+    
+}
